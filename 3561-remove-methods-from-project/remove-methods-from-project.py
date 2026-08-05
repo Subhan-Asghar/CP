1aa4750d0f2953237c136e7b521ca831bf4945e3
Subhan Asghar <subhanasghar16@gmail.com>
2026-08-05 12:40:29 -0700

    Time: 309 ms (63.24%) | Memory: 141.1 MB (39.71%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.py
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -1,35 +1,21 @@
 from collections import defaultdict
 class Solution:
     def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
-        D={}
-        for i in range(n):
-            D[i]=[]
+        D=defaultdict(list)
         for u,v in invocations:
             D[u].append(v)
-        
+
         suspicious=set()
+
         def dfs(node,visited):
             visited.add(node)
             for nei in D[node]:
                 if nei not in visited:
                     dfs(nei,visited)
         dfs(k,suspicious)
+    
+        for u,v in invocations:
+            if u not in suspicious and v in suspicious:
+                return list(range(n))
+        return [i for i in range(n) if i not in suspicious]
 
-        not_suspicious=set()
-
-        def dfs2(node,visited):
-            visited.add(node)
-            for nei in D[node]:
-                if nei in suspicious:
-                    return True
-                if nei not in visited:
-                    if dfs2(nei,visited):
-                        return True
-            return False
-                
-        for node in list(D):
-            if node not in suspicious:
-                if dfs2(node,not_suspicious):
-                    return list(range(n))
-        return list(not_suspicious)
-        
