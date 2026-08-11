76ddcb57af82570ddb66ce7030f4ed56a84cfde0
Subhan Asghar <subhanasghar16@gmail.com>
2026-08-11 12:28:14 -0700

    Time: 161 ms (37.93%) | Memory: 74.2 MB (20.47%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,31 +1,17 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        # def knapsack(W,n):
-        #     if n==0:
-        #         if W==0:
-        #             return 1
-        #         return 0
-        #     if nums[n-1]>W:
-        #         ans=knapsack(W,n-1)
-        #     else:
-        #         nega=knapsack(W-nums[n-1],n-1)
-        #         plus=knapsack(W+nums[n-1],n-1)
-        #         ans=nega+plus
-        #     return ans
-        # return knapsack(target,len(nums))
+        n=len(nums)
         memo={}
-        def func(W,n):
-            if n==len(nums):
-                if W==target:
+        def solve(index,amount):
+            if index==n:
+                if amount==target:
                     return 1
                 return 0
-            if (W,n) in memo:
-                return memo[(W,n)]
-            plus=func(W+nums[n],n+1)
-            nega=func(W-nums[n],n+1)
-            ans=plus+nega
-            memo[(W,n)]=ans
+            if (index,amount) in memo:
+                return memo[(index,amount)]
+            neg=solve(index+1,amount-nums[index])
+            pos=solve(index+1,amount+nums[index])
+            ans=neg+pos
+            memo[(index,amount)]=ans
             return ans
-        return func(0,0)
-
-
+        return solve(0,0)
