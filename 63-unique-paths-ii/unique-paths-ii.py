fe4e5b7a405ed90897711476d89464721dbd4acc
Subhan Asghar <subhanasghar16@gmail.com>
2026-08-11 12:08:08 -0700

    Time: 3 ms (20.65%) | Memory: 19.7 MB (6.93%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,18 +1,21 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        r=len(obstacleGrid)
+        c=len(obstacleGrid[0])
+        if obstacleGrid[r-1][c-1]==1:
+            return 0
         memo={}
-        def func(i,j):
-            if i<0 or j<0 or i>=r or j>=c or  obstacleGrid[i][j]==1:
-                return 0
+        def solve(i,j):
             if (i,j) in memo:
                 return memo[(i,j)]
+            if i<0 or j<0 or i>=r or j>=c or obstacleGrid[i][j]==1:
+                return 0
             if i==r-1 and j==c-1:
                 return 1
-            down=func(i+1,j)
-            right=func(i,j+1)
+            down=solve(i+1,j)
+            right=solve(i,j+1)
             ans=down+right
             memo[(i,j)]=ans
             return ans
-        r=len(obstacleGrid)
-        c=len(obstacleGrid[0])
-        return func(0,0)
+        return solve(0,0)
+            
