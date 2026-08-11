f2b650bb606af4a69c75f408f233dca031acf2a9
Subhan Asghar <subhanasghar16@gmail.com>
2026-08-11 12:34:41 -0700

    Time: 752 ms (50.19%) | Memory: 160.1 MB (19.26%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,32 +1,22 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total=sum(nums)
-        if total%2!=0:
+        total_sum=sum(nums)
+        if total_sum%2!=0:
             return False
-        target=total//2
+        target=total_sum//2
         n=len(nums)
-        # def knapsack(W,n):
-        #     if W==0:
-        #         return True
-        #     if n==0:
-        #         return False
-        #     if nums[n-1]>W:
-        #         if knapsack(W,n-1):
-        #             return True
-        #     else:
-        #         if knapsack(W-nums[n-1],n-1):
-        #             return True
-        #         if knapsack(W,n-1):
-        #             return True
-        #     return False
-        # return knapsack(target,n)
-        
-        dp=[False]*(target+1)
-        dp[0]=True
-        for i in range(n):
-            for w in range(target, nums[i] - 1, -1):
-                take=dp[w - nums[i]]
-                skip=dp[w]
-
-                dp[w] = take or skip
-        return dp[target]
+        memo={}
+        def solve(index,amount):
+            if amount==target:
+                return True
+            if  amount>target or index==n:
+                return False
+            if (index,amount) in memo:
+                return memo[(index,amount)]
+            if solve(index+1,amount+nums[index]):
+                return True
+            if solve(index+1,amount):
+                return True
+            memo[(index,amount)]=False
+            return False
+        return solve(0,0)
