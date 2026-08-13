26f74531d7f982625b50d6202a12cd1e11ab53d3
Subhan Asghar <subhanasghar16@gmail.com>
2026-08-13 01:25:04 -0700

    Time: 3412 ms (10.49%) | Memory: 27 MB (15.50%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -1,22 +1,27 @@
+import math
 class Solution:
     def numSquares(self, n: int) -> int:
         nums=[]
         i=1
-        while i * i <= n:
-            nums.append(i * i)
-            i += 1
-        def knapsack(remain):
-            if remain==0:
-                return 0
-            if remain in memo:
-                return memo[remain]
-            ans=float('inf')
-        
-            for n in nums:
-                if n>remain:
-                    break
-                ans=min(ans,1+knapsack(remain-n))
-            memo[remain]=ans
-            return ans
+        while i*i<=n:
+            nums.append(i*i)
+            i+=1
+        W=n
         memo={}
-        return knapsack(n)
+        def solve(W):
+            if W==0:
+                return 0
+
+            if W in memo:
+                return memo[W]
+
+            ans=float('inf')
+
+            for n in nums:
+                if n>W:
+                    break
+                take=1+solve(W-n)
+                ans=min(ans,take)
+            memo[W]=ans
+            return ans
+        return solve(W)
