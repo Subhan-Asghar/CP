c879ffd1928e2c482f7977a5179a1e2d26c49db3
Subhan Asghar <subhanasghar16@gmail.com>
2026-08-13 01:54:46 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.11%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,14 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def knapsack(n):
-            if n<=0:
-                return 0
-            if n in memo:
-                return memo[n]
-            take=nums[n-1]+knapsack(n-2)
-            skip=knapsack(n-1)
-            ans=max(take,skip)
-            memo[n]=ans
-            return ans
+        n=len(nums)
         memo={}
-        return knapsack(len(nums))
+        def solve(index):
+            if index>=n:
+                return 0
+            if index in memo:
+                return memo[index]
+            take=nums[index]+solve(index+2)
+            skip=solve(index+1)
+            ans=max(take,skip)
+            memo[index]=ans
+            return ans
+        return solve(0)
