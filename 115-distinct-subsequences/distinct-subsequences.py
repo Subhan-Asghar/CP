27b31f6a202e8b98b5d250fa3a73b44c6ee74087
Subhan Asghar <subhanasghar16@gmail.com>
2026-08-15 14:03:43 -0700

    Time: 791 ms (15.38%) | Memory: 217.4 MB (20.19%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,20 +1,20 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         memo={}
-        def solve(m,n,text):
-            if text==t:
+        def solve(m,n):
+            if n==0:
                 return 1
-            if m==0 or n==0:
+            if m==0:
                 return 0
-            if (m,n,text) in memo:
-                return memo[(m,n,text)]
+            if (m,n) in memo:
+                return memo[(m,n)]
             ans=0
             if s[m-1]==t[n-1]:
-                ans+=solve(m-1,n-1,t[n-1]+text)
-                ans+=solve(m-1,n,text)
+                ans+=solve(m-1,n-1)
+                ans+=solve(m-1,n)
             else:
-                ans+=solve(m-1,n,text)
-            memo[(m,n,text)]=ans
+                ans+=solve(m-1,n)
+            memo[(m,n)]=ans
             return ans
-        return solve(len(s),len(t),"")
+        return solve(len(s),len(t))
         
