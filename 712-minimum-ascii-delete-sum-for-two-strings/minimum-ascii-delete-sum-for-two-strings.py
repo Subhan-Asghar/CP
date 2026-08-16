113703d854d1617d44e0dc736728405394c13a90
Subhan Asghar <subhanasghar16@gmail.com>
2026-08-16 04:25:29 -0700

    Time: 658 ms (18.94%) | Memory: 179.4 MB (16.73%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        val1=0
+        val2=0
+        for s in s1:
+            val1+=ord(s)
+        for s in s2:
+            val2+=ord(s)
+        memo={}
+        def solve(m,n):
+            if m==0 or n==0:
+                return 0
+            if (m,n) in memo:
+                return memo[(m,n)]
+            if s1[m-1]==s2[n-1]:
+                ans=ord(s1[m-1])+solve(m-1,n-1)
+            else:
+                ans=max(solve(m-1,n),solve(m,n-1))
+            memo[(m,n)]=ans
+            return ans
+        ans=solve(len(s1),len(s2))
+        result=(val1-ans)+(val2-ans)
+        return result
