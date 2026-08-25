1d512179e8caf97a9acc27abb395956ccc386477
Subhan Asghar <subhanasghar16@gmail.com>
2026-08-25 02:12:56 -0700

    Time: 2859 ms (23.23%) | Memory: 237.2 MB (7.36%) - LeetSync

--- 1808-stone-game-vii/stone-game-vii.py
+++ 1808-stone-game-vii/stone-game-vii.py
@@ -3,21 +3,23 @@ class Solution:
         n=len(stones)
         prefix=[0]*n
         prefix[0]=stones[0]
+
         for i in range(1,n):
             prefix[i]=prefix[i-1]+stones[i]
-        memo = [[None] * n for _ in range(n)]
+        memo=[[None]*n for _ in range(n)]
         def solve(l,r):
             if l>=r:
                 return 0
             if memo[l][r] is not None:
                 return memo[l][r]
             if l==0:
-                amount=prefix[r]
+                val=prefix[r]
             else:
-                amount=prefix[r]-prefix[l-1]
-            first=(amount-stones[l])-solve(l+1,r)
-            second=(amount-stones[r])-solve(l,r-1)
-            ans=max(first,second)
+                val=prefix[r]-prefix[l-1]
+            left=(val-stones[l])-solve(l+1,r)
+            right=(val-stones[r])-solve(l,r-1)
+            ans=max(left,right)
             memo[l][r]=ans
             return ans
-        return solve(0,len(stones)-1)
+        return solve(0,n-1)
+        
