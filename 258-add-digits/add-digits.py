1f2ba4bc4dcbd9dc506913814664b9548556d162
Subhan Asghar <subhanasghar16@gmail.com>
2026-09-03 23:28:20 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (54.48%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -1,9 +1,5 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        s=str(num)
-        while len(s)>1:
-            ans=0
-            for i in s:
-                ans+=int(i)
-            s=str(ans)
-        return int(s)
+        if num==0:
+            return 0
+        return 1+(num-1)%9
